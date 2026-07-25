e3d8dccb8a82db94207004c9619b193918b0e2a1
pischayas <pischayas@gmail.com>
2026-07-26 00:46:37 +0700

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import NumberEquationGame from './NumberEquationGame'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <NumberEquationGame />
-  </React.StrictMode>
-)
