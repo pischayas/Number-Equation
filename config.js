505f6f78f0634389d618f5e10112c0d429ba4bce
pischayas <pischayas@gmail.com>
2026-08-12 16:32:20 +0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// config.example.js
-// คัดลอกไฟล์นี้ไปเป็น config.js แล้วใส่ค่าจริงของคุณ
-window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyDHIdotLc224L6W_Y0rPmfpLWSmOCJrBes",
-  authDomain: "number-equation.firebaseapp.com",
-  databaseURL: "https://number-equation-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "number-equation",
-};
